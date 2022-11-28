9386e91852cec828dcad2fa2f758ea6524740e84
Maximo Espinal <maximoespinaldm@gmail.com>
2022-11-28 16:04:45 -0400

    assets folder landingpage complete

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -6,8 +6,18 @@ function index() {
   return (
     <>
       <Navbar />
-      <Project foto="https://images.unsplash.com/photo-1549740425-5e9ed4d8cd34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwzOTU0NTB8fGVufDB8fHx8&w=1000&q=80" date='2015' tittle='work tittle' work='freelance'/>
-      <Project foto="https://11pluslondon.co.uk/app/uploads/2020/06/AdobeStock_303789965-online-2-min-1372x600.jpeg" />
+      <Project
+        foto="https://images.unsplash.com/photo-1549740425-5e9ed4d8cd34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwzOTU0NTB8fGVufDB8fHx8&w=1000&q=80"
+        date="2018"
+        tittle="A fish in your ear"
+        work="Google Translate"
+      />
+      <Project
+        foto="https://11pluslondon.co.uk/app/uploads/2020/06/AdobeStock_303789965-online-2-min-1372x600.jpeg"
+        date="2018"
+        tittle="A fish in your ear"
+        work="Google Translate"
+      />
     </>
   );
 }
